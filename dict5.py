bf3cac581dcdbd7b3a8f5a6754e1885dc636af44
Max Anderson <maxanderson006@gmail.com>
2018-08-27 21:12:14 -0500

    playing with dictionary

---
+++ dict5.py
@@ -0,0 +1,15 @@
+vowels = [ 'a', 'e', 'i', 'o', 'u' ]
+
+word = input("Enter a word to search for a vowel: ")
+
+found = {}
+
+
+for letter in word:
+    if letter in vowels:
+        found.setdefault(letter, 0)
+        found[letter] += 1
+
+for k, v in sorted(found.items()):
+    print(k, 'was found', v, 'time(s)')
+
